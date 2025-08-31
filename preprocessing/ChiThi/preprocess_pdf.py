3a3f035313161b676ea1de271431877815dddbf1
Le Tuan Dat <141851162+letuandatt@users.noreply.github.com>
2025-08-31 14:18:38 +0700

    Fix: close vectorstore trước khi out filter_retriever. Add: init_session

--- preprocessing/ChiThi/preprocess_pdf.py
+++ preprocessing/ChiThi/preprocess_pdf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b165bdd550b3b458b4ccf49605ff52f08bae6c4a69de1b453ba0ae3f04d2e7da
-size 25138
+oid sha256:a70d43fee7806651d0e9054509b7f856c70a142a03dfc71d2a4dd839bc6d232a
+size 25533
